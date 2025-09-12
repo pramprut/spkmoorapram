cd6eedbbe059a2dcda3b3e76e26edd167177a402
Pramudya Abimanyu <pramangga123@gmail.com>
2025-09-12 20:23:34 +0700

    light

--- content.php
+++ content.php
@@ -70,7 +70,7 @@ if ($_SESSION['level'] == "") {
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
