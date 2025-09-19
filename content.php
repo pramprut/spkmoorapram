8f6b0389b39a06b9a13d66dc5f9684080c00a5bd
Pramudya Abimanyu <pramangga123@gmail.com>
2025-09-19 16:36:41 +0700

    Update content.php
    title

--- content.php
+++ content.php
@@ -14,7 +14,7 @@ if ($_SESSION['level'] == "") {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SSB PSAD Brawijaya Surabaya</title>
+  <title>SPK Puri Surya Jaya</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
